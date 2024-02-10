4c3c7b04043a00579f7601e05f2ce48ab497f1e5
Mason Pyle <mason.d.pyle@gmail.com>
2024-02-10 15:47:29 -0500

    Adding Code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int dice1 = rollDice(random);
+            int dice2 = rollDice(random);
+            int sum = dice1 + dice2;
+
+            System.out.println("Rolling the dice...");
+            System.out.println("You rolled: " + dice1 + " + " + dice2 + " = " + sum);
+
+            if (sum == 2 || sum == 3 || sum == 12) {
+                System.out.println("Craps! You lose!");
+            } else if (sum == 7 || sum == 11) {
+                System.out.println("Natural! You win!");
+            } else {
+                System.out.println("The point is: " + sum);
+                int point = sum;
+                boolean continueRolling = true;
+
+                while (continueRolling) {
+                    System.out.print("Press Enter to roll the dice again...");
+                    scanner.nextLine();
+
+                    dice1 = rollDice(random);
+                    dice2 = rollDice(random);
+                    sum = dice1 + dice2;
+
+                    System.out.println("Rolling the dice...");
+                    System.out.println("You rolled: " + dice1 + " + " + dice2 + " = " + sum);
+
+                    if (sum == 7) {
+                        System.out.println("You rolled a 7. You lose!");
+                        continueRolling = false;
+                    } else if (sum == point) {
+                        System.out.println("You made the point (" + point + ")! You win!");
+                        continueRolling = false;
+                    } else {
+                        System.out.println("You didn't make the point (" +point+ ").");
+                    }
+                }
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainResponse = scanner.nextLine();
+            playAgain = playAgainResponse.equalsIgnoreCase("yes");
         }
+    }
+
+    private static int rollDice(Random random) {
+        return random.nextInt(6) + 1; // Returns a random number between 1 and 6
     }
 }
